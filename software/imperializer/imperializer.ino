02d369e245e1183a53e69c21511e890b526d9109
Andy Lustig <alustig3@gmail.com>
2020-06-06 13:48:31 -0400

    increments only down downstroke. switch moves the number between left and right side.

--- software/imperializer/imperializer.ino
+++ software/imperializer/imperializer.ino
@@ -12,6 +12,10 @@ int counter = 0;
 int offset = 0;
 bool right = true;
 
+bool lastMode = false;
+bool currentMode = false;
+
+
 void keyPressed() {
     interrupted = true;
 }
@@ -25,35 +29,46 @@ void setup() {
   pinMode(switch,INPUT_PULLUP);
 
   Wire.begin();
-	as.init(8, 1);
+	as.init(8, 13);
 	as.clear();
   as.read(); // reset any pending interrupt on the chip side
-
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (interrupted){
-    counter++;
     interrupted = false;
-    delay(150);
-    uint8_t areg = ~as.readPort(0);
-    uint8_t breg = ~as.readPort(1);
-    Serial.print(areg,BIN);
-    Serial.print("\t");
-    Serial.println(breg,BIN);
-    as.display(1 + 4*right, counter/1000);
-    as.display(2 + 4*right, counter/100);
-    as.display(3 + 4*right, counter/10+128);
-    as.display(4 + 4*right, counter%10);
+    uint8_t regA = ~as.readPort(0);
+    uint8_t regB = ~as.readPort(1);
+    uint16_t keyReg = regA<<8 | regB;
+    Serial.println(keyReg,HEX);
+    if (keyReg){
+      counter++;
+      putNumber(counter);
+    }
   }
 
-  if(digitalRead(switch)){
-    right = false;
+  currentMode = digitalRead(switch);
+  delay(50);
+  if(lastMode!=currentMode){
+    if(digitalRead(switch)){
+      putNumber(0);
+      right = false;
+      putNumber(counter);
+    }
+    else{
+      putNumber(0);
+      right = true;
+      putNumber(counter);
+    }
   }
-  else{
-    right = true;
-  }
+  lastMode = currentMode;
 
+}
+
+void putNumber(int number){
+  as.display(1 + 4*right, number%10000/1000);
+  as.display(2 + 4*right, number%1000/100);
+  as.display(3 + 4*right, number%100/10+128);
+  as.display(4 + 4*right, number%10);
 }
