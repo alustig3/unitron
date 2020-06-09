1ba5ed349104f01c99f5736b6842914b0b2daf5f
Andy Lustig <alustig3@gmail.com>
2020-06-08 23:24:23 -0400

    decimal input works now!

--- software/imperializer/imperializer.ino
+++ software/imperializer/imperializer.ino
@@ -18,13 +18,13 @@
 #define nine 0x4000
 #define dot 0x2000
 
-#define bright 13
 #define dim 2
+#define bright 15
 
 AS1115 as = AS1115(0x00);
 
 volatile bool interrupted = false;
-long counter = 0;
+double counter = 0;
 int offset = 0;
 bool right = true;
 
@@ -36,13 +36,16 @@ bool altBlink = false;
 
 uint8_t digit = 0;
 
+bool dotAdded = false;
+int decimalPlace = 0;
 
 void keyPressed() {
-    interrupted = true;
+  interrupted = true;
 }
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("Startup...");
 
   pinMode(AS1115_ISR_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(AS1115_ISR_PIN), keyPressed, FALLING);
@@ -53,6 +56,9 @@ void setup() {
 	as.init(8, bright);
 	as.clear();
   as.read(); // reset any pending interrupt on the chip side
+
+  displayConversion();
+  Serial.println("done");
 }
 
 void loop() {
@@ -62,73 +68,68 @@ void loop() {
     uint8_t regA = ~as.readPort(0);
     uint8_t regB = ~as.readPort(1);
     uint16_t keyReg = regA<<8 | regB;
-    // Serial.println(keyReg,HEX);
+
     if (keyReg){
-      counter *= 10;
-      switch (keyReg){
-        case zero:
-          counter += 0;break;
-        case one:
-          counter += 1;break;
-        case two:
-          counter += 2;break;
-        case three:
-          counter += 3;break;
-        case four:
-          counter += 4;break;
-        case five:
-          counter += 5;break;
-        case six:
-          counter += 6;break;
-        case seven:
-          counter += 7;break;
-        case eight:
-          counter += 8;break;
-        case nine:
-          counter += 9;break;
-        case dot:
-          counter = 0;break;
-      }
-      if(digitalRead(SWITCH)){
-        putNumber(counter*1.0,0);
-        putNumber(counter*25.4,1);
+      if (dotAdded){
+        decimalPlace--;
+        Serial.println(decimalPlace);
       }
       else{
-        putNumber(counter/25.4,0);
-        putNumber(counter*1.0,1);
+        counter *= 10;
       }
-
-      // float rslt = counter/25.4*1000;
-      // Serial.print(rslt);
-      // Serial.print("\t");
-      // Serial.print(int(rslt));
-      // Serial.print("\t");
-      // Serial.print(int(rslt)%1000);
-      // Serial.println();
+      switch (keyReg){
+        case one:
+          counter += 1*pow(10,decimalPlace);break;
+        case two:
+          counter += 2*pow(10,decimalPlace);break;
+        case three:
+          counter += 3*pow(10,decimalPlace);break;
+        case four:
+          counter += 4*pow(10,decimalPlace);break;
+        case five:
+          counter += 5*pow(10,decimalPlace);break;
+        case six:
+          counter += 6*pow(10,decimalPlace);break;
+        case seven:
+          counter += 7*pow(10,decimalPlace);break;
+        case eight:
+          counter += 8*pow(10,decimalPlace);break;
+        case nine:
+          counter += 9*pow(10,decimalPlace);break;
+        case dot:
+          if (dotAdded){
+            counter = 0;
+            dotAdded = false;
+            decimalPlace = 0;
+          }
+          else if (counter){
+            dotAdded = true;
+            counter/=10;
+          }
+          break;
+      }
+      Serial.println(counter);
+      displayConversion();
     }
   }
 
   currentMode = digitalRead(SWITCH);
   delay(50);
   if(lastMode!=currentMode){
-    if(currentMode){
-      putNumber(counter*1.0,!currentMode);
-      putNumber(counter*25.4,currentMode);
-    }
-    else{
-      putNumber(counter/25.4,currentMode);
-      putNumber(counter*1.0,!currentMode);
-    }
+    displayConversion();
+    altBlink = !altBlink;
+    blinkDigit();
   }
   lastMode = currentMode;
 
-  if (blinkTimer++ > 5){
+  if (blinkTimer++ > 2){
     blinkDigit();
     blinkTimer = 0;
   }
 }
 
-void putNumber(float result, bool right){
+
+void putNumber(double result, bool right){
   uint8_t decimal = 0;
   unsigned long number;
   unsigned long dispNum = (unsigned long)(result*100);
@@ -168,7 +169,12 @@ void putNumber(float result, bool right){
   }
 
   if (number >= 0){
-    as.display(4 + 4*right, number%10);
+    if ((dotAdded==true) && (decimalPlace==0)){
+      as.display(4 + 4*right, number%10 +128);
+    }
+    else{
+      as.display(4 + 4*right, number%10);
+    }
   }
   else{
     as.display(4 + 4*right, BLANK);
@@ -178,13 +184,41 @@ void putNumber(float result, bool right){
 void blinkDigit(){
   if (altBlink){
     as.setBankIntensity(!currentMode,dim);
-    // putNumber(-1,!currentMode);
-    // as.setIntensity(2);// blink all eight
+    as.setBankIntensity(currentMode,bright);
+    // as.setIntensity(dim);// blink all eight
   }
   else{
     as.setBankIntensity(!currentMode,bright);
-    // putNumber(counter,!currentMode);
-    // as.setIntensity(13);
+    // as.setIntensity(bright);
   }
   altBlink = !altBlink;
+}
+
+void displayConversion(){
+  if(currentMode){
+    putNumber(counter*1.0,!currentMode);
+    putNumber(getMM(counter),currentMode);
+    // putNumber(getC(counter),currentMode);
+  }
+  else{
+    putNumber(getIN(counter),currentMode);
+    // putNumber(getF(counter),currentMode);
+    putNumber(counter*1.0,!currentMode);
+  }
+}
+
+double getMM(float input){
+  return input*25.4;
+}
+
+double getIN(float input){
+  return input/25.4;
+}
+
+double getF(float input){
+  return input*9/5.0 + 32;
+}
+
+double getC(float input){
+  return (input-32)*5/9.0;
 }
