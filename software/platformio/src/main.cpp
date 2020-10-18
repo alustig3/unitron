502eb8a948a10b0be52b5817a5d9692abea2cd23
Andy Lustig <alustig3@gmail.com>
2020-10-18 16:01:41 -0400

    v1.2 features added
    soft latch works. press to turn on, hold to turn off
    can cycle through conversion modes. corresponding indicator LEDs will light up
    can specify number of decimals and if number is negative.
    sends keypresses to computer as keyboard device

--- software/platformio/src/main.cpp
+++ software/platformio/src/main.cpp
@@ -1,10 +1,14 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <AS1115.h>
+#include "Keyboard.h"
 
-#define AS1115_ISR_PIN  4 ///< Interrupt pin connected to AS1115
-#define SWITCH 3
+
+#define AS1115_ISR_PIN  8 ///< Interrupt pin connected to AS1115
+#define SWITCH 9
+#define BTN 14
+
 #define BLANK 15
-
 #define zero 0x1000
 #define one 0x40
 #define two 0x20
@@ -16,9 +20,10 @@
 #define eight 0x80
 #define nine 0x4000
 #define dot 0x2000
+#define negative 0xA
 
 #define dim 1
-#define bright 15
+#define bright 5 
 
 AS1115 as = AS1115(3);
 AS1115 as2 = AS1115(1);
@@ -41,17 +46,39 @@ int decimalPlace = 0;
 
 double fractions[5] = {1,.1,.01,.001,.0001};
 unsigned long multipliers[9] = {1,10,100,1000,10000,100000,1000000,1000000,10000000};
-byte places = 4;
 
 unsigned long clear_timer;
+unsigned int btn_hold_timer = 0;
+
+int LED_1 = 11;
+int LED_2 = 13;
+int LED_3 = 10;
+int LED_4 = 12;
+int toggle = 9;
+
+int options[4]  = {LED_1, LED_2, LED_3, LED_4};
+int current_option = 0;
 
 void keyPressed() {
   interrupted = true;
 }
 
+void displayConversion();
+void blinkDigit();
+double getMM(float input);
+double getIN(float input);
+double getMILE(float input);
+double getKM(float input);
+double getLB(float input);
+double getKG(float input);
+float getF(float input);
+float getC(float input);
+
+
 void setup() {
-  Serial.begin(115200);
-  Serial.print("Startup...");
+  pinMode(BTN, INPUT_PULLUP);
+  SerialUSB.begin(115200);
+  SerialUSB.print("Startup...");
 
   pinMode(AS1115_ISR_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(AS1115_ISR_PIN), keyPressed, FALLING);
@@ -63,12 +90,23 @@ void setup() {
   as.selfAddress();
 	as.init(8, bright);
   as2.init(8, bright);
+  as.resume(true);
+  as2.resume(true);
   
   as.read(); // reset any pending interrupt on the chip side
   as2.read(); // reset any pending interrupt on the chip side
 
   displayConversion();
-  Serial.println("done");
+
+  for (int i=0; i<4; i++){
+    pinMode(options[i],OUTPUT);
+  }
+  digitalWrite(options[0],HIGH);
+  SerialUSB.println("done");
+  Keyboard.begin();
+
+  while((!digitalRead(BTN))){;;}
+
 }
 
 void loop() {
@@ -85,26 +123,46 @@ void loop() {
       }
       switch (keyReg){
         case zero:
+          Keyboard.print(0);
           break;
         case one:
-          counter += 1*fractions[decimalPlace];break;
+          counter += 1*fractions[decimalPlace];
+          Keyboard.print(1);
+          break;
         case two:
-          counter += 2*fractions[decimalPlace];break;
+          counter += 2*fractions[decimalPlace];
+          Keyboard.print(2);
+          break;
         case three:
-          counter += 3*fractions[decimalPlace];break;
+          counter += 3*fractions[decimalPlace];
+          Keyboard.print(3);
+          break;
         case four:
-          counter += 4*fractions[decimalPlace];break;
+          counter += 4*fractions[decimalPlace];
+          Keyboard.print(4);
+          break;
         case five:
-          counter += 5*fractions[decimalPlace];break;
+          counter += 5*fractions[decimalPlace];
+          Keyboard.print(5);
+          break;
         case six:
-          counter += 6*fractions[decimalPlace];break;
+          counter += 6*fractions[decimalPlace];
+          Keyboard.print(6);
+          break;
         case seven:
-          counter += 7*fractions[decimalPlace];break;
+          counter += 7*fractions[decimalPlace];
+          Keyboard.print(7);
+          break;
         case eight:
-          counter += 8*fractions[decimalPlace];break;
+          counter += 8*fractions[decimalPlace];
+          Keyboard.print(8);
+          break;
         case nine:
-          counter += 9*fractions[decimalPlace];break;
+          counter += 9*fractions[decimalPlace];
+          Keyboard.print(9);
+          break;
         case dot:
+          Keyboard.print('.');
           if (millis() - clear_timer < 400){
             counter = 0;
             dotAdded = false;
@@ -125,7 +183,7 @@ void loop() {
           }
           break;
       }
-      Serial.print(~keyReg & 0xFFFF,BIN);Serial.print(",");Serial.println(keyReg & 0xFFFF,HEX);
+      SerialUSB.print(~keyReg & 0xFFFF,BIN);SerialUSB.print(",");SerialUSB.println(keyReg & 0xFFFF,HEX);
       displayConversion();
     }
   }
@@ -143,9 +201,39 @@ void loop() {
     blinkDigit();
     blinkTimer = 0;
   }
+  if (!digitalRead(BTN)) {
+    int count = 0;
+    while (!digitalRead(BTN)){
+      count++;
+      if (count>15){
+        as.shutdown(true);
+        as2.shutdown(true);
+        pinMode(BTN,OUTPUT);
+        digitalWrite(BTN,LOW);
+        delay(1000);
+      }
+      delay(50);
+      if (blinkTimer++ > 2){
+        blinkDigit();
+        blinkTimer = 0;
+      }
+    }
+    digitalWrite(options[current_option],LOW);
+    current_option++; 
+    if (current_option>3){
+      current_option = 0;
+    }
+    digitalWrite(options[current_option],HIGH);
+    delay(50);
+    blinkTimer++;
+    displayConversion();
+  }
+  else{
+    btn_hold_timer = 0;
+  }
 }
 
-void putNumber(double result, AS1115 *segDisp){
+void putNumber(double result, AS1115 *segDisp,byte places, bool isNegative = false){
   unsigned long whole_num = (unsigned long)result;
   unsigned long decimal_num = (result - whole_num)*multipliers[places+1];
 
@@ -177,6 +265,7 @@ void putNumber(double result, AS1115 *segDisp){
     decimal_num = decimal_num/10;
   }
   //show whole number
+  bool checkedForNegative = false;
   for (int i = decimals_used; i<8; i++){
     if (whole_num){
       segDisp->display(8-i, whole_num % 10 + decimal);
@@ -189,9 +278,16 @@ void putNumber(double result, AS1115 *segDisp){
       else{
         segDisp->display(8-i, BLANK + decimal);
       }
+      if (!checkedForNegative){
+        if (isNegative){
+          segDisp->display(8-i,negative);
+        }
+        checkedForNegative = true;
+      }
     }
     decimal = 0;
   }
+
 }
 
 void blinkDigit(){
@@ -213,18 +309,58 @@ void blinkDigit(){
       as.setIntensity(bright);
     }
   }
-
   altBlink = !altBlink;
 }
 
 void displayConversion(){
-  if(!currentMode){
-    putNumber(counter*1.0, &as2);
-    putNumber(getMM(counter),&as);
-  }
-  else{
-    putNumber(counter*1.0,&as);
-    putNumber(getIN(counter),&as2);
+  switch (current_option){
+    case 0:
+      if(!currentMode){
+        putNumber(getMM(counter),&as,4);
+        putNumber(counter*1.0, &as2,4);
+      }
+      else{
+        putNumber(counter*1.0,&as,4);
+        putNumber(getIN(counter),&as2,4);
+      }
+      break;
+    case 1:
+      if(!currentMode){
+        putNumber(getKM(counter),&as,2);
+        putNumber(counter*1.0, &as2,2);
+      }
+      else{
+        putNumber(counter*1.0,&as,2);
+        putNumber(getMILE(counter),&as2,2);
+      }
+      break;
+    case 2:
+      if(!currentMode){
+        putNumber(getKG(counter),&as,2);
+        putNumber(counter*1.0, &as2,2);
+      }
+      else{
+        putNumber(counter*1.0,&as,2);
+        putNumber(getLB(counter),&as2,2);
+      }
+      break;
+    case 3:
+      if(!currentMode){
+        putNumber(counter*1.0, &as2,1);
+        float temp = getC(counter);
+        if (temp<0){
+          temp *= -1;
+          putNumber(temp,&as,1,true);
+        }
+        else{
+          putNumber(temp,&as,1);
+        }
+      }
+      else{
+        putNumber(counter*1.0,&as,1);
+        putNumber(getF(counter),&as2,1);
+      }
+      break;
   }
 }
 
@@ -236,11 +372,11 @@ double getIN(float input){
   return input/25.4;
 }
 
-double getF(float input){
+float getF(float input){
   return input*9/5.0 + 32;
 }
 
-double getC(float input){
+float getC(float input){
   return (input-32)*5/9.0;
 }
 
